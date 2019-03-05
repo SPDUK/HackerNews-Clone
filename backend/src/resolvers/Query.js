9ac7b2ef94c7907b10b81be3cf531fefe8ffbbf0
SPDUK <spdevuk@gmail.com>
2019-03-05 18:29:48 +0000

    feat: add filtering for feed query

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,7 +1,23 @@
 const Query = {
   info: () => `This is the API of a Hackernews Clone`,
-  feed(root, args, context, info) {
-    return context.prisma.links();
+
+  async feed(parent, args, context, info) {
+    // If no filter string is provided, then the where object will be just an empty object and no
+    // filtering conditions will be applied by the Prisma engine when it returns the response for
+    // the links query.
+    const where = args.filter
+      ? {
+          OR: [
+            { description_contains: args.filter },
+            { url_contains: args.filter },
+          ],
+        }
+      : {};
+
+    const links = await context.prisma.links({
+      where,
+    });
+    return links;
   },
 };
 
