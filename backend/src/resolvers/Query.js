e6c272c11f0d8c0c060df0977353638ec8df6157
SPDUK <spdevuk@gmail.com>
2019-03-05 18:42:20 +0000

    feat: add pagination to feed

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,21 +1,20 @@
 const Query = {
   info: () => `This is the API of a Hackernews Clone`,
 
-  async feed(parent, args, context, info) {
+  async feed(parent, { filter, skip, first }, context, info) {
     // If no filter string is provided, then the where object will be just an empty object and no
     // filtering conditions will be applied by the Prisma engine when it returns the response for
     // the links query.
-    const where = args.filter
+    const where = filter
       ? {
-          OR: [
-            { description_contains: args.filter },
-            { url_contains: args.filter },
-          ],
+          OR: [{ description_contains: filter }, { url_contains: filter }],
         }
       : {};
 
     const links = await context.prisma.links({
       where,
+      skip,
+      first,
     });
     return links;
   },
