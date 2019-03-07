7ffb7f3d2ee74e7440a7635525962da448d228c1
SPDUK <spdevuk@gmail.com>
2019-03-07 16:17:18 +0000

    chore: remove console.log

--- backend/src/utils/getUserId.js
+++ backend/src/utils/getUserId.js
@@ -1,7 +1,6 @@
 const jwt = require('jsonwebtoken');
 
 function getUserId(context) {
-  console.log(context.request.get('Authorization'));
   const Authorization = context.request.get('Authorization');
   if (Authorization) {
     const token = Authorization.replace('Bearer ', '');
