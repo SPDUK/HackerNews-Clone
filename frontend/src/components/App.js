b26be3dde5fe92f23e4c07e6dfdeae60252eb6b0
SPDUK <spdevuk@gmail.com>
2019-03-07 19:27:22 +0000

    fix: update the width container to be mobile-friendly

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -4,18 +4,20 @@ import Header from './Header';
 import LinkList from './LinkList';
 import CreateLink from './CreateLink';
 import Login from './Login';
+import Search from './Search';
 import '../styles/App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="center w85">
+      <div className="center">
         <Header />
         <div className="ph3 pv1 background-gray">
           <Switch>
             <Route exact path="/" component={LinkList} />
             <Route exact path="/create" component={CreateLink} />
             <Route exact path="/login" component={Login} />
+            <Route exact path="/search" component={Search} />
           </Switch>
         </div>
       </div>
