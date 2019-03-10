2a4fbb7880bbfe907229598cfc1e39fec8d8e28e
SPDUK <spdevuk@gmail.com>
2019-03-10 13:04:07 +0000

    feat: add subscription to feed to get live updates when a post is made

--- frontend/src/components/LinkList.js
+++ frontend/src/components/LinkList.js
@@ -27,6 +27,27 @@ const FEED_QUERY = gql`
   }
 `;
 
+const NEW_LINKS_SUBSCRIPTION = gql`
+  subscription {
+    newLink {
+      id
+      url
+      description
+      createdAt
+      postedBy {
+        id
+        name
+      }
+      votes {
+        id
+        user {
+          id
+        }
+      }
+    }
+  }
+`;
+
 class LinkList extends Component {
   updateStoreAfterVote = (store, createVote, linkId) => {
     const data = store.readQuery({ query: FEED_QUERY });
@@ -37,13 +58,37 @@ class LinkList extends Component {
     store.writeQuery({ query: FEED_QUERY, data });
   };
 
+  subscribeToNewLinks = subscribeToMore => {
+    subscribeToMore({
+      // fire every time a new link is created
+      document: NEW_LINKS_SUBSCRIPTION,
+      // determines how the store should be updated based on previous state
+      updateQuery: (prev, { subscriptionData }) => {
+        if (!subscriptionData.data) return prev;
+        const newLink = subscriptionData.data.newLink;
+        const exists = prev.feed.links.find(({ id }) => id === newLink.id);
+        if (exists) return prev;
+
+        return Object.assign({}, prev, {
+          feed: {
+            links: [newLink, ...prev.feed.links],
+            count: prev.feed.links.length + 1,
+            __typename: prev.feed.__typename,
+          },
+        });
+      },
+    });
+  };
+
   render() {
     return (
       <>
         <Query query={FEED_QUERY}>
-          {({ loading, error, data }) => {
+          {({ loading, error, data, subscribeToMore }) => {
             if (loading) return <div>Loading...</div>;
             if (error) return <div>Error</div>;
+
+            this.subscribeToNewLinks(subscribeToMore);
 
             return (
               <div className="ph3 pv1 background-gray">
@@ -66,4 +111,4 @@ class LinkList extends Component {
 }
 
 export default LinkList;
-export { FEED_QUERY };
+export { FEED_QUERY, NEW_LINKS_SUBSCRIPTION };
