ec27dd754a6a69eb8547d69725cf5a24cdd64208
SPDUK <spdevuk@gmail.com>
2019-03-14 08:08:08 +0000

    fix: use wss instead of ws

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,4 +1,4 @@
 export const LINKS_PER_PAGE = 30;
 export const AUTH_TOKEN = 'auth-token';
 export const FRONTEND_URL = 'https://backend-dwjxqgirot.now.sh/';
-export const FRONTEND_WS_URL = 'ws://backend-dwjxqgirot.now.sh/';
+export const FRONTEND_WS_URL = 'wss://backend-dwjxqgirot.now.sh/';
